d9c91798ba17b5b249ab69e48726c457a667fb20
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2025-02-11 15:12:21 +0100

    add js

---
+++ script.js
@@ -0,0 +1,12 @@
+let form = document.querySelector('form');
+let nom = document.querySelector('nom');
+let prenom = document.querySelector('prenom');
+let email = document.querySelector('email');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+
+});
